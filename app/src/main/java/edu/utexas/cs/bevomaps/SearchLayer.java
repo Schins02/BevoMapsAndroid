c274c3b4723891dc024b000556839b2b5267ddcc
Steven Zvonek <skz94@cs.utexas.edu>
2015-04-16 17:10:21 -0500

    Update SearchLayer.java
    Removed unnecessary toLowerCase() because the string was already lower cased.

--- app/src/main/java/edu/utexas/cs/bevomaps/SearchLayer.java
+++ app/src/main/java/edu/utexas/cs/bevomaps/SearchLayer.java
@@ -24,7 +24,7 @@ class SearchLayer {
     //Map of common searches to the building or floor
     Map<String,String> cache_map = c.loadSearchMap();
     for (String t : strings) {
-      if (cache_map.containsKey(t.toLowerCase())) {
+      if (cache_map.containsKey(t)) {
         s = s.replaceAll(t, cache_map.get(t).toLowerCase());
       }
     }
